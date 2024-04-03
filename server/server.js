3c6cd30efa3aab919b98de0cf64826b141dbfc30
EMcNaughton-WestMec <142362842+EMcNaughton-WestMec@users.noreply.github.com>
2024-04-03 00:27:45 +0000

    4/2/2024

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const path = require('path');
+const fs = require('fs');
+// const cors = require('cors');
 const app = express();
+// app.use(cors);
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.get('/csv-data', (req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        } else {
+            res.type('text/plain');
+            res.send(data);
+        }
+    });
 });
 
 app.get('/myles', (req, res) => {
